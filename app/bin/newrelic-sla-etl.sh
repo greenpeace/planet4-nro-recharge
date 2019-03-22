1575101c903f1a389363b6f8410b1703887b027c
Raymond Walker <raymond.walker@greenpeace.org>
2019-03-22 15:04:04 +1100

    Add script to transform NewRelic data to BQ table schema

---
+++ app/bin/newrelic-sla-etl.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+nro="${1:-$RECHARGE_BUCKET_PATH}"
+range="${2:-${RECHARGE_PERIOD}}"
+
+input_file="${3:-$RECHARGE_OUTPUT_FILE}"
+
+output_dir="$(dirname "$input_file")/transform"
+mkdir -p "$output_dir"
+
+echo "Converting NewRelic output to BigQuery table format ..."
+
+# Application
+output_file="$output_dir/appdex-$(basename "$input_file")"
+
+jq -cM \
+  --arg nro "$nro" \
+  --arg range "$range" \
+  '.metric_data
+  | del(.metrics_not_found,.metrics_found)
+  + { range: $range, nro: $nro, from: .from[0:-6], to: .to[0:-6] }
+  + .metrics[0].timeslices[0].values
+  | del(.metrics)' "$input_file" | tee "$output_file"
+
+# End user
+output_file="$output_dir/enduser-$(basename "$input_file")"
+
+jq -cM \
+  --arg nro "$nro" \
+  --arg range "$range" \
+  '.metric_data
+  | del(.metrics_not_found,.metrics_found)
+  + { range: $range, nro: $nro, from: .from[0:-6], to: .to[0:-6] }
+  + .metrics[1].timeslices[0].values
+  | del(.metrics)' "$input_file" | tee "$output_file"
