0017907078ec5474a62c3b66fe717076bdb8ff6d
Raymond Walker <raymond.walker@greenpeace.org>
2019-03-26 10:51:11 +1100

    Switch to DATE data type, rename variables

--- app/bin/newrelic-sla-etl.sh
+++ app/bin/newrelic-sla-etl.sh
@@ -2,38 +2,39 @@
 set -euo pipefail
 
 nro="${1:-$RECHARGE_BUCKET_PATH}"
-range="${2:-${RECHARGE_PERIOD}}"
+period="${2:-${RECHARGE_PERIOD}}"
 
 input_file="${3:-$RECHARGE_OUTPUT_FILE}"
 
 output_dir="$(dirname "$input_file")/transform"
 mkdir -p "$output_dir"
 
+# Application SLA data
 echo "Converting NewRelic output to BigQuery table format ..."
-
-# Application
 output_file="$output_dir/appdex-$(basename "$input_file")"
 
+# Manipulate data to match BQ schema
 jq -cM \
   --arg nro "$nro" \
-  --arg range "$range" \
+  --arg period "$period" \
   '.metric_data
   | del(.metrics_not_found,.metrics_found)
-  + { range: $range, nro: $nro, from: .from[0:-6], to: .to[0:-6] }
+  + { period: $period, nro: $nro, from: .from[0:10], to: .to[0:10] }
   + .metrics[0].timeslices[0].values
   | del(.metrics)' "$input_file" | tee "$output_file"
 
 bq-store.sh "$output_file" newrelic_appdex
 
-# End user
+# End user SLA data
 output_file="$output_dir/enduser-$(basename "$input_file")"
 
+# Manipulate data to match BQ schema
 jq -cM \
   --arg nro "$nro" \
-  --arg range "$range" \
+  --arg period "$period" \
   '.metric_data
   | del(.metrics_not_found,.metrics_found)
-  + { range: $range, nro: $nro, from: .from[0:-6], to: .to[0:-6] }
+  + { period: $period, nro: $nro, from: .from[0:10], to: .to[0:10] }
   + .metrics[1].timeslices[0].values
   | del(.metrics)' "$input_file" | tee "$output_file"
 
