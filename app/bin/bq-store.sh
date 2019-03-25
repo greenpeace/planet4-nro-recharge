223491c28b7a8731f3a6984a1b0d3187791e3f0c
Raymond Walker <raymond.walker@greenpeace.org>
2019-03-26 10:47:00 +1100

    Append schema version to bq tables

--- app/bin/bq-store.sh
+++ app/bin/bq-store.sh
@@ -8,9 +8,10 @@ dataset="${3:-${RECHARGE_BQ_DATASET}}"
 project="${4:-${RECHARGE_PROJECT_ID}}"
 
 echo
-echo "Storing $input_file in table: $dataset.$table ..."
+echo "Storing $input_file in table: $dataset.${table}_v${SCHEMA_VERSION} ..."
 echo
 
-bq \
-  --project_id="$project" \
-  load --source_format=NEWLINE_DELIMITED_JSON "$dataset.$table" "$input_file"
+bq load \
+  --autodetect \
+  --source_format=NEWLINE_DELIMITED_JSON \
+  "$dataset.${table}_v${SCHEMA_VERSION}" "$input_file"
