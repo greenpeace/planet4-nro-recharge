fef7fbf9166b2ec648f88971158784d7325c8904
Raymond Walker <raymond.walker@greenpeace.org>
2019-03-26 10:51:33 +1100

    ISO8601 date format

--- app/bin/newrelic-sla-get.sh
+++ app/bin/newrelic-sla-get.sh
@@ -3,21 +3,9 @@ set -eo pipefail
 
 app_id=${1:-${NEWRELIC_APP_ID}}
 
-## Determine NewRelic application ID from name if not set
-if [ -z "${NEWRELIC_APP_ID}" ] && [ -n "${NEWRELIC_APP_NAME}" ]
-then
-  app_id=$(newrelic-get-application-id.sh)
-fi
-
-if [ -z "$app_id" ]
-then
-  >&2 echo "Error: NEWRELIC_APP_ID not set"
-  exit 1
-fi
-
 set -u
 
-range=${2:-"from=${DATE_START}Z00:00:00+00:00&to=${DATE_END}Z23:59:59+00:00"}
+range=${2:-"from=${DATE_START}T00:00:00+00:00Z&to=${DATE_END}T23:59:59+00:00Z"}
 
 # https://docs.newrelic.com/docs/apm/reports/service-level-agreements/api-examples-sla-reports
 mkdir -p /tmp
