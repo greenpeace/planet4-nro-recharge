17d75b62041b7703645bc66dbdfea83eb25d96fc
Raymond Walker <raymond.walker@greenpeace.org>
2019-03-26 10:50:19 +1100

    Remove unnecessary project variable. It's defined in .bigqueryrc

--- app/bin/bq-store.sh
+++ app/bin/bq-store.sh
@@ -5,7 +5,6 @@ input_file="${1}"
 table="${2}"
 
 dataset="${3:-${RECHARGE_BQ_DATASET}}"
-project="${4:-${RECHARGE_PROJECT_ID}}"
 
 echo
 echo "Storing $input_file in table: $dataset.${table}_v${SCHEMA_VERSION} ..."
