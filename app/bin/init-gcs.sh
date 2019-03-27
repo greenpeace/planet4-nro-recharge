757f0f3701b78c920aebdcde01ebcbd2daaa54c1
Raymond Walker <raymond.walker@greenpeace.org>
2019-03-27 12:12:01 +1100

    Syntax tweak

--- app/bin/init-gcs.sh
+++ app/bin/init-gcs.sh
@@ -6,19 +6,19 @@ set -euo pipefail
 
 init_recharge_bucket() {
   # Make bucket if it doesn't exist
-  gsutil ls -p "${RECHARGE_PROJECT_ID}" "gs://${RECHARGE_BUCKET_NAME}" >/dev/null || {
-    echo "Initialising WP Stateless bucket"
-    echo
-    echo "Project: ${RECHARGE_PROJECT_ID}"
-    echo "Labels:"
-    echo " - app:         planet4-nro-recharge"
-    echo " - environment: production"
-    echo " - component:   newrelic"
-    echo "Bucket:  gs://${RECHARGE_BUCKET_NAME}"
-    echo "Region:  us"
+  gsutil ls -p "${RECHARGE_PROJECT_ID}" "gs://${RECHARGE_BUCKET_NAME}" >/dev/null && return
 
-    gsutil mb -l "${RECHARGE_BUCKET_LOCATION}" -p "${RECHARGE_PROJECT_ID}" "gs://${RECHARGE_BUCKET_NAME}"
-  }
+  echo " * gcs: Initialising WP Stateless bucket"
+  echo
+  echo " * gcs: Project: ${RECHARGE_PROJECT_ID}"
+  echo " * gcs: Labels:"
+  echo " * gcs:  - app:         planet4-nro-recharge"
+  echo " * gcs:  - environment: production"
+  echo " * gcs:  - component:   newrelic"
+  echo " * gcs: Bucket:  gs://${RECHARGE_BUCKET_NAME}"
+  echo " * gcs: Region:  us"
+
+  gsutil mb -l "${RECHARGE_BUCKET_LOCATION}" -p "${RECHARGE_PROJECT_ID}" "gs://${RECHARGE_BUCKET_NAME}"
 
   # Apply labels
   gsutil label ch \
@@ -26,23 +26,24 @@ init_recharge_bucket() {
     -l "environment:production" \
     -l "component:recharge" \
     "gs://${RECHARGE_BUCKET_NAME}"
+
 }
 
 init_gcs_usage_bucket() {
   # Make bucket if it doesn't exist
-  gsutil ls -p "${RECHARGE_PROJECT_ID}" "gs://${USAGE_BUCKET_NAME}" >/dev/null || {
-    echo "Initialising WP Stateless bucket"
-    echo
-    echo "Project: ${RECHARGE_PROJECT_ID}"
-    echo "Labels:"
-    echo " - app:         planet4-nro-recharge"
-    echo " - environment: production"
-    echo " - component:   usage"
-    echo "Bucket:  gs://${USAGE_BUCKET_NAME}"
-    echo "Region:  us"
+  gsutil ls -p "${RECHARGE_PROJECT_ID}" "gs://${USAGE_BUCKET_NAME}" >/dev/null && return
 
-    gsutil mb -l "${RECHARGE_BUCKET_LOCATION}" -p "${RECHARGE_PROJECT_ID}" "gs://${USAGE_BUCKET_NAME}"
-  }
+  echo " * gcs: Initialising WP Stateless bucket"
+  echo
+  echo " * gcs: Project: ${RECHARGE_PROJECT_ID}"
+  echo " * gcs: Labels:"
+  echo " * gcs:  - app:         planet4-nro-recharge"
+  echo " * gcs:  - environment: production"
+  echo " * gcs:  - component:   usage"
+  echo " * gcs: Bucket:  gs://${USAGE_BUCKET_NAME}"
+  echo " * gcs: Region:  us"
+
+  gsutil mb -l "${RECHARGE_BUCKET_LOCATION}" -p "${RECHARGE_PROJECT_ID}" "gs://${USAGE_BUCKET_NAME}"
 
   # Apply lifecycle configuration
   gsutil lifecycle set lifecycle.json "gs://${USAGE_BUCKET_NAME}"
@@ -59,8 +60,10 @@ init_gcs_usage_bucket() {
 
   # Set default ACL of objects to private
   gsutil defacl set project-private "gs://${USAGE_BUCKET_NAME}"
+
 }
 
 # Retrying here because gsutil is flaky, connection resets often
 retry init_recharge_bucket
+
 retry init_gcs_usage_bucket
