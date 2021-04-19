0fda07819edb54690168f06439a1a31931322f08
Gus Minto-Cowcher <gmintoco@greenpeace.org>
2021-04-19 19:17:36 +1000

    double quote

--- app/bin/init-gcs.sh
+++ app/bin/init-gcs.sh
@@ -77,7 +77,7 @@ function create_application_id_file() {
   secret=$(echo "$deployment" | cut -d'-' -f1-2)-db
 
   describe=$(kubectl -n "$ns" describe deployment "$name")
-  mysql_user=$(kubectl -n "$ns" get secret $secret --template="{{.data.username | base64decode }}")
+  mysql_user=$(kubectl -n "$ns" get secret "$secret" --template="{{.data.username | base64decode }}")
 
   [ -z "$describe" ] && {
     >&2 echo "ERROR: deployment not found: $deployment"
